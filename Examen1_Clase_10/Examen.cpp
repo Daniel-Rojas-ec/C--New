8ac296d09286ca9db126be41dee815b004c083f4
Daniel Rojas <alejandro.r.u.ec@gmail.com>
2026-05-13 22:03:43 -0500

    clase10-Incxomp

--- Examen1_Clase_10/Examen.cpp
+++ Examen1_Clase_10/Examen.cpp
@@ -34,36 +34,85 @@ float calcularpromedio (float notas[], int cantidad);
 void notamayorymenor(string estidantes [], float notas[], int cantidad);
 void contaraprobadosoreprobados(float notas[], int cantidad);
 void buscarestudiante (string estudiantes[], float notas[], int cantidad);
-void mostrarmenu();
+void mostrarmenu(int opcion);
  
 //Metemos la funcion principal
 
 int main()
 {
     //Declaramos las variables
-    float notas[10];
-    string estudiantes[10];
+    int i;
+    int opcion;
+    float notas[10]; 
+    string estudiantes[10]; 
     int cantidad;
-    float promedio;
-    
-    mostrarmenu();
-    registrardatos(notas, estudiantes, cantidad);
-    promedio = calcularpromedio (notas, cantidad);
-    notamayorymenor(estudiantes, notas, cantidad);
-    contaraprobadosoreprobados(notas, cantidad);
-    buscarestudiante(estudiantes, notas,cantidad);
+    float suma=0;
+
+    mostrarmenu(opcion);
 
     return 0;
 }
 
-void mostrarmenu ()
+void registrardatos (float notas[], string estudiantes[], int &cantidad)
 {
+    cout<<"Ingrese la cantidad de estudiantes"<<endl;
+    cin>>cantidad;
+    cout<<"Ingrese los estudiantes"<<endl;
+    cin>>estudiantes[10];
+    cout<<"Ingrese las notas"<<endl;
+    cin>>notas[10];
+}
+
+float calcularpromedio (float notas[], int cantidad, float suma)
+{
+    int i;
+    if(cantidad==0)
+    {
+        cout<< "No existe estudiantes"<<endl;
+    }
+    else 
+    {
+        for (i=0;i<cantidad;i++)
+        suma=suma+notas[i];
+    cout<<"El promedio de los estudiantes es"<<endl;
+    }
+
+    return suma/10;
+}
 
 
 
 
+
+
+
+
+
+
+
+
+void mostrarmenu (int opcion)
+{
+    do
+    {
+        cout<<"=========== MENU ==========="<<endl;
+        cout<<"1. Registrar estudiantes y notas"<<endl;
+        cout<<"2. Mostrar listado completo"<<endl;
+        cout<<"3. Mostrar promedio general"<<endl;
+        cout<<"4. Mostrar nota mayor y nota menor"<<endl;
+        cout<<"5. Mostrar aprobados y reprobados"<<endl;
+        cout<<"6. Buscar estudiante por nombre"<<endl;
+        cout<<"7. Salir"<<endl;
+        cout<<"Seleccione una opcion"<<endl;
+        cin>>opcion;
 
     
+
+
+        
+
+        
+    }while (opcion != 7);
 }
 
 
