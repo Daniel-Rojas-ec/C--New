eae7115370aca845e15e6a7c674e24da46341520
Daniel Rojas <alejandro.r.u.ec@gmail.com>
2026-05-13 21:08:23 -0500

    clase10- Declarar firmas de funciones e inicar en funcion principal

---
+++ Examen1_Clase_10/Examen.cpp
@@ -0,0 +1,61 @@
+/*
+Nombre del estudiante: ______________________________
+Curso: Bootcamp Programación Cero a POO
+Clase: 10 - Examen 1 (C++)
+Fecha: 18/03/2026
+Tema:
+Sistema de registro y análisis de notas de estudiantes
+Entrada:
+- Cantidad de estudiantes
+- Nombres de estudiantes
+- Nota de cada estudiante
+Proceso:
+- Validar datos ingresados
+- Almacenar información en arreglos
+- Calcular promedio general
+- Buscar una nota mayor y una nota menor
+- Mostrar reporte de aprobados y reprobados
+- Permitir consultar un estudiante por nombre
+Salida:
+- Listado de estudiantes y notas
+- Promedio general
+- Nota mayor y nota menor
+- Cantidad de aprobados y reprobados
+- Resultado de búsqueda por nombre
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Escribirmos la firma de cada fucion
+
+void registrardatos (float notas[], string estudiantes[], int cantidad []);
+float calcularpromedio (float notas[], int cantidad[]);
+void notamayorymenor(string estidantes [], float notas[], int cantidad[]);
+void contaraprobadosoreprobados(float notas[], int cantidad);
+void buscarestudiante (string estudiante[], float notas, int cantidad[]);
+void mostrarmenu();
+ 
+//Metemos la funcion principal
+
+int main()
+{
+    //Declaramos las variables
+    float notas[10];
+    string estudiantes[10];
+    int cantidad;
+    float promedio;
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
