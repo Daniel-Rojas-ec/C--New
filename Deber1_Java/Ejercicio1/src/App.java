7ce063239cf0c95ffa132a9bb82eab7de420ad31
Daniel Rojas <alejandro.r.u.ec@gmail.com>
2026-05-20 23:44:55 -0500

    Deber1Java-Creamos los atributos y el constructor Ej01

---
+++ Deber1_Java/Ejercicio1/src/App.java
@@ -0,0 +1,20 @@
+/*
+Titulo: Deber 1 en java.
+Ejercicio 1
+Enunciado
+Crea una clase llamada Mascota que represente una mascota sencilla.
+La clase debe tener los siguientes atributos:
+• nombre
+• especie
+• edad
+*/
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
+    
+    }
+    
+    
+}
