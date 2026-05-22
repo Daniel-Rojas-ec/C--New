08dab65096e50b5093340f71d51d640e9467d527
Daniel Rojas <alejandro.r.u.ec@gmail.com>
2026-05-22 00:01:47 -0500

    Deber1Java-Crear la clase telefono definir atributos y hacer getters Ej02

---
+++ Java/Deber1_Java/Ejercicio2/src/Telefono.java
@@ -0,0 +1,33 @@
+public class Telefono 
+{
+    private String marca;
+    private String modelo;
+    private double precio;
+
+    public Telefono (String marca, String modelo, double precio)
+    {
+        this.marca=marca;
+        this.modelo=modelo;
+        this.precio=precio;
+    }
+
+    public String getmarca()
+    {
+        return marca;
+    }
+    public String getmodelo()
+    {
+        return modelo;
+    }
+    public double getprecio()
+    {
+        return precio;
+    }
+    
+
+
+
+
+
+
+}
